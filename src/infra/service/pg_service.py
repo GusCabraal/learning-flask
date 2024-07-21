33a4dddd0d543d944fe51a2c3833104f79c1fc2c
cabral <gustavo@acordofechado.com.br>
2024-07-21 17:43:49 -0300

    feat: cria o metodo run query dentro do pg service

--- src/infra/service/pg_service.py
+++ src/infra/service/pg_service.py
@@ -13,42 +13,44 @@ class Database:
 
     @classmethod
     def get_users_from_db(self):
-        cursor = self.connect().cursor()
-        cursor.execute("SELECT * FROM users ORDER BY created_at DESC")
-        users = cursor.fetchall()
-        cursor.close()
-        return users
+        return self.run_query("SELECT * FROM users ORDER BY created_at DESC", [])
 
     @classmethod
     def get_user_from_db_by_id(self, id):
-        cursor = self.connect().cursor()
-        cursor.execute("SELECT * FROM users WHERE id = %s;", [id])
-        user = cursor.fetchone()
-        cursor.close()
-        return user
+        response = self.run_query("SELECT * FROM users WHERE id = %s;", [id])
+        return response[0]
 
     @classmethod
     def create(self, data):
-        conn = self.connect()
-        cursor = conn.cursor()
-        cursor.execute(
+        response = self.run_query(
             "INSERT INTO users (name, cpf) VALUES (%s, %s) RETURNING *",
             (data["name"], data["cpf"]),
         )
-        conn.commit()
-        user = cursor.fetchone()
-        cursor.close()
-        return user
+        return response[0]
 
     @classmethod
     def update(self, id, data):
-        conn = self.connect()
-        cursor = conn.cursor()
-        cursor.execute(
+        response = self.run_query(
             "UPDATE users SET name = %s, cpf = %s WHERE id = %s RETURNING *",
             (data["name"], data["cpf"], id),
         )
-        conn.commit()
-        user = cursor.fetchone()
-        cursor.close()
-        return user
+        return response[0]
+
+    def run_query(query, parameters):
+        try:
+            connection = psycopg2.connect(
+                dbname=environ.get("DB_NAME"),
+                host=environ.get("DB_HOST"),
+                password=environ.get("DB_PASS"),
+                user=environ.get("DB_USER"),
+            )
+            cursor = connection.cursor()
+            cursor.execute(query, parameters)
+            connection.commit()
+            return cursor.fetchall()
+        except (Exception, psycopg2.Error) as error:
+            print("Faiure running query", error)
+        finally:
+            if connection:
+                cursor.close()
+                connection.close()
