37e8b4d439b8f063179f6c29c20f7872d898a2f4
cabral <gustavo@acordofechado.com.br>
2024-07-21 15:06:42 -0300

    feat: busca um usuario pelo id

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from infra.db import get_users_from_db
+from infra.db import Database
 from infra.model.user_model import User
 
 
@@ -8,9 +8,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def get_users():
-    users_from_db = get_users_from_db()
+    users_from_db = Database.get_users_from_db()
     users = [User(user).to_dict() for user in users_from_db]
     return jsonify(users)
+
+
+@app.route("/<id>")
+def get_user_by_id(id: int):
+    user_from_db = Database.get_user_from_db_by_id(id)
+    user = User(user_from_db).to_dict()
+    return jsonify(user)
 
 
 def start_server(host: str = "0.0.0.0", port: int = 8000):
--- src/infra/db.py
+++ src/infra/db.py
@@ -2,18 +2,28 @@ from os import environ
 import psycopg2
 
 
-def get_users_from_db():
-    try:
+class Database:
+    def connect():
         conn = psycopg2.connect(
             dbname=environ.get("DB_NAME"),
             host=environ.get("DB_HOST"),
             password=environ.get("DB_PASS"),
             user=environ.get("DB_USER"),
         )
-        cursor = conn.cursor()
+        return conn.cursor()
+
+    @classmethod
+    def get_users_from_db(self):
+        cursor = self.connect()
         cursor.execute("select * from users limit 1")
         users = cursor.fetchmany()
+        cursor.close()
         return users
-    except psycopg2.Error as e:
-        print(e)
-        return e
+
+    @classmethod
+    def get_user_from_db_by_id(self, id):
+        cursor = self.connect()
+        cursor.execute("select * from users where id = %s", (id))
+        user = cursor.fetchone()
+        cursor.close()
+        return user
